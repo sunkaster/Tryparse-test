de24aa59c2a7005784e15a8097c5b27f6c43df55
Sunkaster <54435371+sunkaster@users.noreply.github.com>
2020-11-20 14:23:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Tryparse_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!TryParse(Console.ReadLine(), out resultat))
+                Console.WriteLine("Skriv in en siffra");
+        }
+        public static bool TryParse(string s, out int result)
+        {
+            try
+            {
+                result = int.Parse(s);
+                return true;
+            }
+            catch
+            {
+                result = 0;
+                return false;
+            }
+        }
+    }
+}
